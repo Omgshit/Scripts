8ae00bcaa6a73449f4d11babed9bb96b8cdcbd5a
Omgshit <123563422+Omgshit@users.noreply.github.com>
2025-02-24 01:39:27 -0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,13 @@
+if game.GameId == 5430301679 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/479e9f38148118df2d14fc0795ece576.lua"))()
+elseif game.GameId == 6793832056 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/541d3f86190c83c874e5c8c8d7f08e37.lua"))()
+elseif game.GameId == 994732206 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c023159c4938fed111c326b00bffc6d5.lua"))()
+elseif game.GameId == 4161970303 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/32f568dc2c517d5d93d91be17b423e8a.lua"))()
+elseif game.GameId == 4069560710 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b436b65706cb68da9f012723fbfce36.lua"))()
+elseif game.GameId == 6325068386 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/394954afd12f05778862d5606230aac3.lua"))()
+end
