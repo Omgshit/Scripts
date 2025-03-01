fa4b0f4204a22c0b8f8abdd3042b23c4cc5ed2d7
Omgshit <123563422+Omgshit@users.noreply.github.com>
2025-03-01 20:52:33 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -30,6 +30,8 @@ elseif game.GameId == 6694982457 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8ac2d791a2561c52ca014fcb57a2a3a8.lua"))()
 elseif game.GameId == 3183403065 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1434ae69c28d9c6c45db4b4cbe7ae88c.lua"))()
+elseif game.GameId == 6471449680 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bfa59e124bf57a66349c7340ff203d5d.lua"))()
 else
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a44eb915034273544643d8a2ca439d20.lua"))()
 end
