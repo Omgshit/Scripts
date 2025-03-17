5e762f619ba55c18401f855aec280bafa8d55004
Omgshit <123563422+Omgshit@users.noreply.github.com>
2025-03-17 04:58:37 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/refs/heads/main/Pika.lua"))()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/479e9f38148118df2d14fc0795ece576.lua"))()
